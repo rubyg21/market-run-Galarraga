e3f8b7e4a94acc3e0b8262459bb15ada22323d8c
Rubens Galarraga <rgalarraga121@gmail.com>
2022-07-14 01:20:46 -0300

    agregamos NavBar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './NavBar.css'
+
+function NavBar() {
+  return (
+    <div className='NavBar'>
+
+        <button className='NavBar_boton'> Inicio </button>
+        <button className='NavBar_boton'>  Runinng </button>
+        <button className='NavBar_boton'> Nosotros </button>
+        <button className='NavBar_boton'> Contacto </button>
+
+    </div>
+  )
+}
+
+export default NavBar
