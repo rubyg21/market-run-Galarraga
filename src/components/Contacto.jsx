345ffbafc6786eec0d3c5e03ce03c9382f820bb4
Rubens Galarraga <rgalarraga121@gmail.com>
2022-09-04 19:39:32 -0300

    correcciones

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import "./Contacto.css";
 
 function Contacto() {
-  return <div>Contacto</div>;
+  return (
+    <div className="contacto">
+      <h1> Sport-980 </h1> <br />
+      <strong>
+        <h3>Atención al Cliente</h3> <br />
+      </strong>
+      Montevideo, Uruguay <br />
+      095.523.468 <br />
+      hola@Sport980.uy <br /> <br />
+      LUNES A VIERNES - 10:00Hs - 19:00Hs <br /> SÁBADO - 10:00Hs - 14:00Hs
+    </div>
+  );
 }
 
 export default Contacto;
