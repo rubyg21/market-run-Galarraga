2cfe86da312079d9e9f8fcd20b91d26b1690a426
Rubens Galarraga <rgalarraga121@gmail.com>
2022-09-04 14:44:33 -0300

    agrego nosotros

--- src/components/Nosotros.jsx
+++ src/components/Nosotros.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import  './Nosotros.css'
+
 
 function Nosotros() {
   return (
-    <div>Nosotros</div>
+<>
+<div className='nosotros'>
+<h1>Nosotros</h1>
+
+<br></br>
+<div className='parrafo'> 
+Somos una empresa familiar que nació en 1989 apasionados por el deporte y el bienestar.  <br/> La apertura del primer local en Montevideo Shopping dio el puntapie para un nuevo concepto de tiendas en el mercado. <br /> 
+
+La primer “casa de deportes” del país. Un lugar donde se pueden encontrar las mejores marcas deportivas con la mayor innovación
+
+Tenemos presencia en 3 locales en Montevideo: Nuevocentro Shopping – Portones Shopping – Montevideo Shopping y 1 en Punta del Este.
+
+<br/> Hablamos a través del deporte y la moda a nuestros consumidores para acompañarlos en sus competencias, entrenamientos y estilos de vida!
+
+<br/> De la mano del e-commerce vamos a poder brindar nuestro servicio y experiencia <strong> ESTÉS DONDE ESTÉS. </strong>
+ 
+<br/>
+<br/>
+Nuestra marca es un clásico que acompaña el día a día de los Uruguayos
+</div>
+
+<br/>
+<hr/>
+</div>
+</>
+
+
+    
   )
 }
 
